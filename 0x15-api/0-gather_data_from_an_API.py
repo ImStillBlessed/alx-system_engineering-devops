1a9c888a806345d35972be060e85a27a0a21dc86
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-19 20:17:18 +0100

    task 0 complete

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,22 @@
 This module retrieves fake data and displays it USING
 REST API
 """
+import json
 import sys
 import urllib.request
 
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/todos/1'
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/' + id
 
-    with urllib.request.urlopen(url) as response:
-        json_data = response.json()
-        print(json_data)
+    user = urllib.request.urlopen(url).read()
+    todos = urllib.request.urlopen(url + '/todos').read()
+
+    user_json = json.loads(user.decode('utf-8'))
+    todos_json = json.loads(todos.decode('utf-8'))
+    name = user_json.get('name')
+    complete = [x for x in todos_json if x.get('completed') == True]
+
+    print(f'Employee {name} is done with tasks({len(complete)}/{len(todos_json)}):')
+    for task in complete:
+        print(f'\t {task.get('title')}')
