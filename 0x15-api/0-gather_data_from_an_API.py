d23ec762f47fe4ba66b03e112d834accb21d290c
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-19 20:21:26 +0100

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,8 +17,9 @@ if __name__ == '__main__':
     user_json = json.loads(user.decode('utf-8'))
     todos_json = json.loads(todos.decode('utf-8'))
     name = user_json.get('name')
-    complete = [x for x in todos_json if x.get('completed') == True]
+    complete = [x for x in todos_json if x.get('completed')]
 
-    print(f'Employee {name} is done with tasks({len(complete)}/{len(todos_json)}):')
+    print(f'Employee {name} is done\
+ with tasks({len(complete)}/{len(todos_json)}):')
     for task in complete:
         print(f'\t {task.get('title')}')
